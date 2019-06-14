6440644d855005b01150b6ee0b223d97015c3d41
Edward <edward@nilsson.gt>
2019-06-14 15:10:05 -0600

    Allow origin only to website

--- routes/login.js
+++ routes/login.js
@@ -12,6 +12,8 @@ router.post('/', function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
+  res.header("Access-Control-Allow-Origin", "*");
+
   if (!email || !password) {
       return res.status(401).json({
           errors: {
