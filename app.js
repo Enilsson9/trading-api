e9f5d061eff269bf43bcb6bf35a480c74416309a
Edward <edward@nilsson.gt>
2019-06-14 14:12:46 -0600

    Allow origin only to website

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+// Website you wish to allow to connect
+res.setHeader('Access-Control-Allow-Origin', 'https://project.edwardnilsson.se');
+
+// Request methods you wish to allow
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+// Request headers you wish to allow
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+res.setHeader('Access-Control-Allow-Credentials', true);
+
 
 // don't show the log when it is test
 if (process.env.NODE_ENV !== 'test') {
