b0676dadebf69b18146a33fc6f33c563e0f7671d
Edward <edward@nilsson.gt>
2019-06-20 18:33:07 -0600

    Added microservice

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require('morgan');
 const cors = require('cors');
 
+const stock = require("./stock.js");
+
 const login = require('./routes/login');
 const register = require('./routes/register');
 const update = require('./routes/update');
@@ -48,6 +50,39 @@ app.use('/data', data);
 
 // Start up server
 const server = app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+
+const io = require('socket.io')(server);;
+
+var princessTarta = {
+    name: "gasoline",
+    rate: 1.002,
+    variance: 0.6,
+    startingPoint: 20,
+};
+
+var cakes = [princessTarta];
+
+/*app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});*/
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+setInterval(function () {
+    cakes.map((cake) => {
+        cake["startingPoint"] = stock.getStockPrice(cake);
+        return cake;
+    });
+
+    // console.log(cakes);
+
+    io.emit("stocks", cakes);
+}, 1000);
 
 //export server
 module.exports = server;
